9b8b766f598af6dd753c26d487d0640902c9f72f
AhnSeongHyun <sh84.ahn@gmail.com>
2019-09-22 00:31:44 +0900

    Add dockerfile, entrypoint.sh

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM docker:19.03.2
+LABEL "com.github.actions.name"="Tag Buil Publish Docker"
+LABEL "com.github.actions.description"="Uses the git branch as the docker tag and pushes the container"
+LABEL "com.github.actions.icon"="anchor"
+LABEL "com.github.actions.color"="blue"
+
+LABEL "repository"="https://github.com/AhnSeongHyun/action-tag-docker-build-push"
+LABEL "maintainer"="sh84.ahn@gmail.com"
+
+RUN apk update \
+  && apk upgrade \
+  && apk add --no-cache git
+
+COPY entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -e
+
+if [ -z "${INPUT_REPO_NAME}" ]; then
+  echo "Need Docker Repo Name e.g. ash84/meier"
+  exit 1
+fi
+
+if [ -z "${INPUT_USERNAME}" ]; then
+  echo "Need Docker user name for login"
+  exit 1
+fi
+
+if [ -z "${INPUT_PASSWORD}" ]; then
+  echo "Need Docker password for login"
+  exit 1
+fi
+
+
+
+TAG=$(echo ${GITHUB_REF} | sed -e "s/refs\/tags\///g")
+
+echo ${INPUT_PASSWORD} | docker login -u ${INPUT_USERNAME} --password-stdin 
+
+DOCKERNAME="${INPUT_REPO_NAME}:${TAG}"
+
+docker build --cache-from -t ${DOCKERNAME} .
+docker push ${DOCKERNAME}
+
+
+docker logout
